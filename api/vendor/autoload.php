7adfffee44f6ea48867b30082e49e9cf28f5cf0a
haram-333 <haramnawaz77@gmail.com>
2026-03-04 19:21:23 +0500

    Add PHPMailer manually (no Composer needed)

---
+++ api/vendor/autoload.php
@@ -0,0 +1,5 @@
+<?php
+// Custom autoloader for PHPMailer since Composer is not installed locally
+require_once __DIR__ . '/PHPMailer/PHPMailer/src/Exception.php';
+require_once __DIR__ . '/PHPMailer/PHPMailer/src/PHPMailer.php';
+require_once __DIR__ . '/PHPMailer/PHPMailer/src/SMTP.php';
